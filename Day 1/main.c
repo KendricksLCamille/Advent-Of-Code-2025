cc5f914a910782c62a74134e20f76848f1c22577
kendricks <camilles20049@hotmail.com>
2025-12-16 17:30:39 -0500

    Refactor overflow handling and add output for zero count in main loop.

--- Day 1/main.c
+++ Day 1/main.c
@@ -62,26 +62,38 @@ Analyze the rotations in your attached document. What's the actual password to o
 
 To play, please identify yourself via one of these services:
  */
+#include <assert.h>
+#include <stdio.h>
+
 #include "stdlib.h"
 constexpr int UNDER_OVER_FLOW_FIX = 100;
 
 // Assumes that no rotation is greater 99
 int move_within_0_and_100(const int input){
-    if(input < 0) return input % UNDER_OVER_FLOW_FIX;
-    if(input >= UNDER_OVER_FLOW_FIX) return UNDER_OVER_FLOW_FIX - (input % UNDER_OVER_FLOW_FIX);
-    return input;
+    if(input >= UNDER_OVER_FLOW_FIX) return input % UNDER_OVER_FLOW_FIX;
+    auto output = input;
+    while (output < 0)
+    {
+        output += UNDER_OVER_FLOW_FIX;
+    }
+    return output;
 }
 
 int main(const int argc, char** argv){
     int position = 50;
     int number_of_zeroes = 0;
+
+    // printf("%10s %10s %10s %10s\n", "index", "position", "command", "number of zeroes");
     for(int i = 1; i < argc; i++){
         const char* current = argv[i];
+        // printf("%10d %10s %10d %10d\n", i - 1, current, position, number_of_zeroes);
         const int ticks = atoi((current + 1));
         const char direction = current[0];
         position += direction == 'L'  ? -ticks : ticks;
         position = move_within_0_and_100(position);
+        assert(position >= 0 && position < 100);
         if(position == 0) number_of_zeroes++;
     }
-    return number_of_zeroes;
+    printf("Answer: %10d\n", number_of_zeroes);
+    return 0;
 }
