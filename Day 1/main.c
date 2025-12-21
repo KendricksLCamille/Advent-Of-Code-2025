f5d6015e01264f37f4b903477df849eab69db45a
kendricks <camilles20049@hotmail.com>
2025-12-20 22:06:29 -0500

    Remove unused PART_2 logic and related functions, clean up main loop, and update IDE inspection settings.

--- Day 1/main.c
+++ Day 1/main.c
@@ -3,7 +3,6 @@
 
 #include "stdlib.h"
 constexpr int UNDER_OVER_FLOW_FIX = 100;
-#define PART_2
 int move_within_0_and_100_part_1(const int input){
     if(input >= UNDER_OVER_FLOW_FIX) return input % UNDER_OVER_FLOW_FIX;
     auto output = input;
@@ -12,25 +11,6 @@ int move_within_0_and_100_part_1(const int input){
         output += UNDER_OVER_FLOW_FIX;
     }
     return output;
-}
-
-typedef struct {int position; int number_of_zeroes;} output_t;
-void number_of_times_count_past_zero_part_2(int* _input, int* _number_of_zeroes)
-{
-    auto input = *_input;
-
-    auto abs_input = abs(input);
-    auto mod = abs_input % UNDER_OVER_FLOW_FIX;
-    auto change_in_zero = abs_input / UNDER_OVER_FLOW_FIX;
-    auto new_input = mod;
-    if (input < 0)
-    {
-        new_input = UNDER_OVER_FLOW_FIX - mod;
-        change_in_zero++;
-    }
-
-    *_input = new_input;
-     *_number_of_zeroes += change_in_zero;
 }
 
 int main(const int argc, char** argv){
@@ -44,14 +24,9 @@ int main(const int argc, char** argv){
         const int ticks = atoi((current + 1));
         const char direction = current[0];
         position += direction == 'L'  ? -ticks : ticks;
-
-#ifdef PART_1
         position = move_within_0_and_100_part_1(position);
         assert(position >= 0 && position < 100);
         if(position == 0) number_of_zeroes++;
-#elifdef PART_2
-        number_of_times_count_past_zero_part_2(&position, &number_of_zeroes);
-#endif
         printf("%10d %10s %10d %10d\n", i, current, position, number_of_zeroes);
     }
     printf("Answer: %10d\n", number_of_zeroes);
