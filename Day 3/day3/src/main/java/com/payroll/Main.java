4a8eb97613dd49cf24172b922a56d649ce90a774
kendricks <camilles20049@hotmail.com>
2025-12-23 19:24:26 -0500

    Refactor `findTheLargestNumberInStringPart1` to use `long` for large number handling and streamline `main` logic with `sumAll`.

--- Day 3/day3/src/main/java/com/payroll/Main.java
+++ Day 3/day3/src/main/java/com/payroll/Main.java
@@ -7,9 +7,9 @@ import java.util.function.Function;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    static int findTheLargestNumberInStringPart1(String str){   // 54321
+    static long findTheLargestNumberInStringPart1(String str){   // 54321
         if(str.isEmpty()) throw new IllegalArgumentException("String cannot be empty");
-        if(str.length() <= 2) return Integer.parseInt(str);
+        if(str.length() <= 2) return Long.parseLong(str);
         var charArray = str.toCharArray();  // [5,4,3,2,1]
         Arrays.sort(charArray); // [1,2,3,4,5]
         var largestNumber = charArray[charArray.length - 1]; // 5
@@ -17,14 +17,14 @@ public class Main {
         if(strIndex == str.length()-1) {
             var end = largestNumber;
             largestNumber = charArray[charArray.length - 2];
-            return Integer.parseInt(largestNumber + "" + end);
+            return Long.parseLong(largestNumber + "" + end);
         }
         var largestCharIndex = str.indexOf(largestNumber);
         var nextLargestSubstring = str.substring(largestCharIndex + 1);
         var nextLargestCharArray = nextLargestSubstring.toCharArray();
         Arrays.sort(nextLargestCharArray);
         var nextLargestCharacter = nextLargestCharArray[nextLargestCharArray.length - 1];
-        return Integer.parseInt(largestNumber + "" + nextLargestCharacter);
+        return Long.parseLong(largestNumber + "" + nextLargestCharacter);
     }
 
     static long Part2Helper2(String source){
@@ -48,31 +48,15 @@ public class Main {
         return Long.parseLong(sb.toString());
     }
 
-    static void findTheLargestNumberInStringPart1(String[] args){
-        var sum = 0;
-        for (String arg : args) {
-            int theLargestNumberInStringPart1 = findTheLargestNumberInStringPart1(arg);
-            sum = sum + theLargestNumberInStringPart1;
-        }
-        IO.println("Sum of largest numbers: " + sum);
-    }
-
-    static void findTheLargestNumberInStringPart2(String[] args){
-        long acc = 0;
-        for (String a : args) {
-            var part2Helper = Part2Helper2(a);
-            acc = acc + part2Helper;
-        }
-        IO.println("Sum of largest numbers: " + acc);
-    }
-
     static void sumAll(String[] args, Function<String, Long> function){
-        var sum = Arrays.stream(args).map(function).reduce(Long::sum);
+        var sum = Arrays.stream(args).map(function).reduce(Long::sum).orElse(0L);
         IO.println("Sum: " + sum);
     }
 
     static void main(String[] args) {
-        findTheLargestNumberInStringPart1(args);
-        findTheLargestNumberInStringPart2(args);
+        // part 1
+        sumAll(args, Main::findTheLargestNumberInStringPart1);
+        // part2
+        sumAll(args, Main::Part2Helper2);
     }
 }
