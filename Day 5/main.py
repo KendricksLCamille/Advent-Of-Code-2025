bfad763a2e0eb64f26c0bda296d04ff4587be3d9
kendricks <camilles20049@hotmail.com>
2025-12-26 09:28:50 -0500

    My original solution was valid but the input provided didn't include the new line when pasted for some reason leading to me being off by one. I had to use someone elses solution to find the answer so I could figure out what was wrong with my answer. Like, I said it was a stupid off by one error due to the new line not being there.

--- Day 5/main.py
+++ Day 5/main.py
@@ -1,11 +1,12 @@
 import sys
+from functools import reduce
+
 import interval
 
 
 def merge_intervals(intervals: list[interval.Interval]) -> list[interval.Interval]:
-    sorted_intervals = sorted(intervals, key=lambda i: i.lower_bound, reverse=False)
-    output = [sorted_intervals.pop()]
-    for interval1 in sorted_intervals:
+    output = [intervals[0]]
+    for interval1 in intervals[1:]:
         joined = False
         for i in range(0, len(output)):
             try:
@@ -19,8 +20,8 @@ def merge_intervals(intervals: list[interval.Interval]) -> list[interval.Interva
         if not joined:
             output.append(interval1)
     if len(output) != len(intervals):
-        output = merge_intervals(output)
-    return output
+        return merge_intervals(output)
+    return sorted(intervals, key=lambda j: j.lower_bound, reverse=False)
 
 def part_one(args: list[str]):
     is_fresh: list[interval.Interval] = []
@@ -33,21 +34,33 @@ def part_one(args: list[str]):
                 array = map(int, array)
                 left, right = array
                 is_fresh.append(interval.Interval(left, right))
+                continue
             else:
                 del array
                 is_checking = True
-                is_fresh = merge_intervals(is_fresh)
-        else:
-            value = int(arg)
-            for interval1 in is_fresh:
-                left = interval1.lower_bound
-                right = interval1.upper_bound
-                if left <= value <= right:
-                    fresh += 1
-                    break
-    print(f'Fresh number: {fresh}')
+                is_fresh = merge_intervals(sorted(is_fresh, key=lambda i: i.lower_bound, reverse=False))
+        value = int(arg)
+        for interval1 in is_fresh:
+            left = interval1.lower_bound
+            right = interval1.upper_bound
+            if left <= value <= right:
+                fresh += 1
+                break
+    print(f'Fresh numbers: {fresh}')
 
+def part_two(args: list[str]):
+    is_fresh: list[interval.Interval] = []
+    for arg in [arg for arg in args if '-' in arg]:
+        array = arg.split('-')
+        array = map(int, array)
+        left, right = array
+        is_fresh.append(interval.Interval(left, right))
+    is_fresh = merge_intervals(sorted(is_fresh, key=lambda i: i.lower_bound, reverse=False))
+    _sum = reduce(lambda j,i: j + i.upper_bound - i.lower_bound + 1, is_fresh, 0)
+    print(f'Sum: {_sum}')
 
 part_one(sys.argv[1:])
+part_two(sys.argv[1:])
 
 #781 is to low
+# answer is 782 by modifying "https://github.com/RichRat/pyxercise/blob/master/advent/2025/adv05.py"
