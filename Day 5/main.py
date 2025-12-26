b8a7200e6c1ec5cd262161a1070ae9fb075cbcb8
kendricks <camilles20049@hotmail.com>
2025-12-26 00:45:29 -0500

    Refactor interval handling logic by introducing `merge_intervals` for efficient overlap merging and updating `part_one` to use interval-based checks.

--- Day 5/main.py
+++ Day 5/main.py
@@ -1,8 +1,29 @@
-from enum import Enum
 import sys
+import interval
+
+
+def merge_intervals(intervals: list[interval.Interval]) -> list[interval.Interval]:
+    sorted_intervals = sorted(intervals, key=lambda i: i.lower_bound, reverse=False)
+    output = [sorted_intervals.pop()]
+    for interval1 in sorted_intervals:
+        joined = False
+        for i in range(0, len(output)):
+            try:
+                output_interval = output[i]
+                joined_interval = interval1.join(output_interval)
+                output[i] = joined_interval
+                joined = True
+                break
+            except ArithmeticError:
+                continue
+        if not joined:
+            output.append(interval1)
+    if len(output) != len(intervals):
+        output = merge_intervals(output)
+    return output
 
 def part_one(args: list[str]):
-    is_fresh: dict[int, bool] = {}
+    is_fresh: list[interval.Interval] = []
     is_checking = False
     fresh: int = 0
     for arg in args:
@@ -11,15 +32,22 @@ def part_one(args: list[str]):
                 array = arg.split('-')
                 array = map(int, array)
                 left, right = array
-                for i in range(left, right + 1):
-                    is_fresh[i] = True
+                is_fresh.append(interval.Interval(left, right))
             else:
+                del array
                 is_checking = True
+                is_fresh = merge_intervals(is_fresh)
         else:
             value = int(arg)
-            if is_fresh.get(value):
-                fresh += 1
+            for interval1 in is_fresh:
+                left = interval1.lower_bound
+                right = interval1.upper_bound
+                if left <= value <= right:
+                    fresh += 1
+                    break
     print(f'Fresh number: {fresh}')
 
 
 part_one(sys.argv[1:])
+
+#781 is to low
